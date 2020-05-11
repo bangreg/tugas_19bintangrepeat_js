115f4fecba20569256d34987960886ff3928f5db
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:30:13 +0700

    Tugas 19 Bintang Repeat JS

---
+++ tugas19bintangrepeat.js
@@ -0,0 +1,13 @@
+function ulangBintang(){
+  var kata = "*****"
+  console.log(kata)
+  console.log(kata.repeat(5))
+  console.log(kata.repeat(9))
+  console.log(kata.repeat(13))
+  console.log(kata.repeat(17))
+  console.log(kata.repeat(13))
+  console.log(kata.repeat(9))
+  console.log(kata.repeat(5))
+  console.log(kata)
+}
+ulangBintang();
